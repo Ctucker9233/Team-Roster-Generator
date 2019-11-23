7f4961462f45ec2ca79f5648e1f215ae2ae91503
Ctucker9233 <court.m.tucker@gmail.com>
2019-11-22 18:15:01 -0800

    Refactored classes

--- app.js
+++ app.js
@@ -1,58 +1,86 @@
 const employeeCreator = require("./lib/employee");
 const Manager = require("./lib/manager");
-const inquirer = require("inquirer")
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the employee's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is the employee's id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is the employee's email?"
-        },
-        {
-            type: "list",
-            name: "roll",
-            message: "What is the employee's role?",
-            choices: ["Manager", "Engineer", "Intern"]
-        }
-    ])
-    .then(function ({ name, id, email, roll }) {
-        switch (roll) {
-            case "Manager":
-                {
-                    inquirer
-                        .prompt([
-                            {
-                                type: "input",
-                                name: "officeNumber",
-                                message: "What is the manager's office number?"
-                            }
-                        ])
-                        .then(function ({ officeNumber }) {
-                            const manager = new Manager(name, id, email, officeNumber);
-                        })
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the employee's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the employee's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email?"
+            },
+            {
+                type: "list",
+                name: "roll",
+                message: "What is the employee's role?",
+                choices: ["Manager", "Engineer", "Intern"]
+            }
+        ])
+        .then(function ({ name, id, email, roll }) {
+            switch (roll) {
+                case "Manager":
+                    {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: "input",
+                                    name: "officeNumber",
+                                    message: "What is the manager's office number?"
+                                }
+                            ])
+                            .then(function ({ officeNumber }) {
+                                const manager = new Manager(name, id, email, officeNumber);
+                                console.log(manager);
+                            })
 
-                }
-            case "Engineer":
-                {
-                    inquirer
-                        .prompt([
-                            {
-                                type: "input",
-                                name: "github",
-                                message: "What is the engineer's github page?"
-                            }
-                        ])
-                }
-        }
-    })
+                    }
+                case "Engineer":
+                    {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: "input",
+                                    name: "github",
+                                    message: "What is the engineer's github page?"
+                                }
+                            ])
+                            .then(function ({ github }) {
+                                const engineer = new Engineer(name, id, email, github)
+                                console.log(engineer)
+                            })
+                    }
+                case "Intern":
+                    {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: "input",
+                                    name: "school",
+                                    message: "Where does the intern go to school?"
+                                }
+                            ])
+                            .then(function({ school }){
+                                const intern = new Intern(name, id, email, school)
+                                console.log(intern)
+                            })
+                    }
+                break;
+
+            }
+        })
+}
+
+init();
